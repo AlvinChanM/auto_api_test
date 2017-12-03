df24e1049d2a79228cd184a5518ee714c897dc84
AlvinChanM <miao564103484@163.com>
2017-12-03 21:10:36 +0800

    Revert "Revert "not the end""
    This reverts commit 7af84cf151d71017152a2ced453913830c545de6.

---
+++ tools/operating_excel.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+import xlrd
+class OperatingExcel:
+    def __init__(self, file_name=None, sheet_id=None):
+        if file_name:
+            self.file_name = file_name
+            self.sheet_id = sheet_id
+        else:
+            self.file_name = r"D:\auto_api_test\auto_api_test\case_data\interface.xlsx"
+            self.sheet_id = 0
+        self.data = self.get_data()
+    # 获取sheets的内容
+    def get_data(self):
+        data = xlrd.open_workbook(self.file_name)
+        tables = data.sheets()[self.sheet_id]
+        return tables
+
+    # 获取单元格的行数
+    def get_lines(self):
+        tables = self.data
+        return tables.nrows
+
+    # 获取某一个单元格的内容
+    def get_cell_value(self,row,col):
+        return self.data.cell_value(row,col)
+
+if __name__ == '__main__':
+    oe = OperatingExcel()
+    print oe.get_lines()
---
+++ tools/operating_json.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+import json
+class OperatingJson():
+    # 构造函数
+    def __init__(self):
+        self.data = self.read_data()
+    # 读取json文件
+    def read_data(self):
+        with open(r"D:\auto_api_test\auto_api_test\case_data\case.json") as fp:
+            data = json.load(fp)
+            return data
+
+    # 根据关键字获取数据
+    def get_data(self, id):
+        return self.data[id]
+
+if __name__ == '__main__':
+    oj = OperatingJson()
+    print oj.get_data('login')
