acf2fbf58b1e94d500d6bc9668ccb42aadb860ca
AlvinChanM <miao564103484@163.com>
2017-12-27 22:15:10 +0800

    遗漏的

--- tools/operating_database.py
+++ tools/operating_database.py
@@ -4,4 +4,13 @@ import MySQLdb
 
 class OperatingMysql():
     def __init__(self):
-        self.conn = MySQLdb.connect()
+        self.conn = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user='root',
+            password='miaomiao123',
+            db='kds',
+            charset='utf8'
+            cursorclass=MySQLdb.cursors.DictCursor #返回键值对
+        )
+        self.cur = self.conn.cursor()
